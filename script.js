89065f4e9fd97e6195c29423c02c109af8d8c3be
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2022-01-27 20:00:39 -0300

    finish

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const resultado = document.querySelector('.result')
-
+const resultado = document.querySelector('.result');
+const confirmar = document.querySelector('.igual');
 function insert( valor) {
     resultado.innerHTML += valor
 }
+
+function clean(){
+    resultado.innerHTML = ' ';
+}
+
+function erease (){
+    if(resultado.textContent){
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring( 0, result.length -1);
+    }
+}
+
+function confirma (){
+    if(resultado.textContent != 'Erro') {
+    document.getElementById("resultado").innerHTML= eval(resultado.innerHTML)
+    }
+} 
